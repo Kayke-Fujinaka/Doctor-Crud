8df437d297a5148ca1a571a7abbe08343b79a22d
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-11-14 16:32:44 -0300

    feat: create app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from './configs/ormconfig';
+import { DoctorModule } from './modules/doctor.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DoctorModule, TypeOrmModule.forRoot(config)],
 })
 export class AppModule {}
