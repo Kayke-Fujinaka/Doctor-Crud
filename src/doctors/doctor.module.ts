145ed8df59a81638b01bef13e39554f5dbcd0574
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-11-16 18:40:47 -0300

    feat: import HttpModule and DoctorZipCodeProvider

--- src/doctors/doctor.module.ts
+++ src/doctors/doctor.module.ts
@@ -1,12 +1,14 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DoctorController } from './doctor.controller';
 import { Doctor } from './entities/doctor.entities';
+import { DoctorZipCodeProvider } from './providers/doctor-zipcode-provider';
 import { DoctorService } from './services/doctor.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Doctor])],
+  imports: [HttpModule, TypeOrmModule.forFeature([Doctor])],
   controllers: [DoctorController],
-  providers: [DoctorService],
+  providers: [DoctorService, DoctorZipCodeProvider],
 })
 export class DoctorModule {}
